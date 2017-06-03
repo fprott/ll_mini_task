24055d317df2bd31600f7903e8d040c19d76fdb3
theChaoS <florianprott@gmail.com>
2017-06-03 13:13:24 +0200

    Share

---
+++ lowlevel_framework/source/src/lowlevel/cluster_encoder.java
@@ -0,0 +1,17 @@
+package lowlevel;
+
+/**
+ * Created by theChaoS on 03.06.2017.
+ */
+public class clusterEncoder {
+
+}
+
+
+public class oneHotEncoder extends clusterEncoder{
+
+}
+
+public class binaryEncoder extends clusterEncoder{
+
+}
